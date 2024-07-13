58dc98625a0d9ed4caf77e95978c97fe4eddf7f4
Danilosilvaf <silvadanilof@outlook.com>
2024-07-13 17:26:23 -0300

    ajustando tdd

--- src/test/java/br/com/fiap/postech/soat/techchallenger4/produtos/Techchallenger4ApplicationTests.java
+++ src/test/java/br/com/fiap/postech/soat/techchallenger4/produtos/Techchallenger4ApplicationTests.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
